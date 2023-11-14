1b29e54901ae3e804258d9d745f752752f699302
Newton dos Santos <neewrobert@gmail.com>
2023-11-14 16:03:35 +0100

    fixing a postgres version to avoid conflict with flyway

--- src/main/resources/db/migration/V1__Initial_schema.sql
+++ src/main/resources/db/migration/V1__Initial_schema.sql
@@ -1,9 +1,13 @@
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
-CREATE OR REPLACE FUNCTION generate_ulid() RETURNS uuid
-    AS $$
-SELECT (lpad(to_hex(floor(extract(epoch FROM clock_timestamp()) * 1000)::bigint), 12, '0') || encode(gen_random_bytes(10), 'hex'))::uuid;
-$$ LANGUAGE SQL;
+CREATE OR REPLACE FUNCTION generate_ulid() RETURNS uuid AS $$
+BEGIN
+RETURN (
+        lpad(to_hex(floor(extract(epoch FROM clock_timestamp()) * 1000)::bigint), 12, '0') ||
+        encode(gen_random_bytes(10), 'hex')
+    )::uuid;
+END;
+$$ LANGUAGE plpgsql;
 
 CREATE TABLE users (
                        id UUID PRIMARY KEY DEFAULT generate_ulid(),
